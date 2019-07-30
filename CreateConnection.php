2ea03be2116ea8b0b2c11c5319c059081ed3427e
phamkhoavp <52957433+phamkhoavp@users.noreply.github.com>
2019-07-30 16:21:10 +0700

    Thay Doi

---
+++ CreateConnection.php
@@ -0,0 +1,18 @@
+<?php 
+	$servername = "db4free.net";
+	$port = "8889";
+	$username = "phamkhoavp";
+	$password = "phamkhoa391999";
+	$dbname = "gwcourses";
+
+	
+	$conn = new mysqli($servername, $username, 
+		$password, $dbname);
+	
+	if ($conn->connect_error) {
+    	die("Connection failed: " . $conn->connect_error);
+	}	 
+
+	echo "done";
+
+ ?>
