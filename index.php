e1744a9eadbc5b823ca77077003ea7217db3e149
phamkhoavp <52957433+phamkhoavp@users.noreply.github.com>
2019-08-06 14:35:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Hello Heroku</title>
+	<title></title>
 </head>
 <body>
-   <?php
-   echo "Hello Heroku PHP !!!";
-   ?>
+	<?php
+	// $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', 'phamkhoa391999');
+	$db = parse_url(getenv("DATABASE_URL"));
+
+    $pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+    ));
+
+
+	$sql = "SELECT studentname, course, dob, gender, fav FROM registercourse";
+	$stmt = $pdo->prepare($sql);
+	//Thiết lập kiểu dữ liệu trả về
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$stmt->execute();
+	$resultSet = $stmt->fetchAll();
+	?>
+	<ul>
+	  <?php
+		foreach ($resultSet as $row) {
+			echo '<li>'.
+			$row['studentname'] . ' -- ' . $row['course'] . ' -- ' . $row['dob'] . ' -- ' . $row['gender'] . ' -- ' . $row['fav'] . '</li>';
+		}
+	  ?>
+	</ul>
 </body>
 </html>
